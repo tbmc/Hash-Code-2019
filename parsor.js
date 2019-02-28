f6bea905f265946d113916bb12f446726a549de7
Thibault BALLIER <thibault.ballier@axa.fr>
2019-02-28 19:06:16 +0100

    parsor

---
+++ parsor.js
@@ -0,0 +1,23 @@
+const { open } = require("./open");
+
+function createFromLine(line) {
+    const lineSplit = line.split(" ");
+    const tags = lineSplit.slice(2);
+    return {
+        tags,
+        id: lineSplit[1],
+        layout: lineSplit[0]
+    };
+}
+
+function parse(inputFile) {
+    const data = open(inputFile);
+    const usefullData = data.slice(1);
+
+    return usefullData.map(createFromLine);
+}
+
+module.exports = parse;
+
+// const data = parse("in/a_example.txt");
+// debugger;
