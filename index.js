a23aa6e58237134f638ac9da236c9631d8c944e5
DUFRENE Guillaume <guillaume.dufrene@axa.fr>
2019-02-28 19:39:51 +0100

    plop

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
+
+
 var fs = require('fs');
 var createInterface = require('readline').createInterface;
 var createReadStream = fs.createReadStream;
 var EOL = require('os').EOL;
-var _ = require('lodash')
+var _ = require('lodash');
 
-var in = require('open')
+var parse = require('./parsor') 
+
 
 var files = [
-  '',
-  '',
-  '',
-  ''
+  'a_example',
+  'b_lovely_landscapes',
+  'c_memorable_moments',
+  'd_pet_pictures',
+  'e_shiny_selfies'
 ];
 
 /*
@@ -25,8 +29,18 @@ var files = [
 
 
 
-var filename = 'in/' + files[0] + '.in'
+var filename = 'in/' + files[1] + '.txt'
 // var lineReader = createInterface({input: createReadStream(filename)});
+var photos = parse( filename )
+/*
+[
+{
+  id: 1,
+  layout: 'H',
+  tags: ['toto', 'titi']
+}]
+*/
+
 
 var count = 0;
 var desc = [];
@@ -34,4 +48,46 @@ var desc = [];
 _ints = (line) => line.split(' ').map( i => parseInt(i) );
 zip = (...rows) => [...rows[0]].map((_,c) => rows.map(row => row[c]))
 
+
+var solution = []
+
+
+var allTags = {};
+
+var available = {};
+_.forEach(photos, function(photo, key) {
+  available[photo.id] = photo
+});
+
+
+var findCandidate = (tag) => {
+  console.log( tag )
+  var i = _.findIndex( available, photo => _.findIndex(photo.tags, tag) >= 0 )
+  return available[i]
+}
+
+function computeTagsOccurs() {
+  _.forEach(photos, function(photo, key) {
+    _.forEach( photo.tags, k => {
+      if ( !allTags[k] ) allTags[k] = 0;
+      allTags[k] += 1;
+    });
+  });
+}
+
+/*
+_.forEach(allTags, function(tag, key) {
+  console.log( key + " = " + tag )
+});
+*/
+
+var solution = []
+
+var current = _.drop( available )
+solution.push( [ current.id ] )
+delete( available[current.id] )
+
+
+var next = findCandidate( current )
+console.log ( next )
 
